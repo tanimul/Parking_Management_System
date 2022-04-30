a33af3bca1d7133197c4e82770ad6504fa9bf303
tanimul <tanimulislam.fuad@gmail.com>
2022-04-30 22:52:03 +0600

    Splash Activity Added.

---
+++ app/src/main/java/com/example/parkingmanagementsystem/ui/intro/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.example.parkingmanagementsystem.ui.intro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.parkingmanagementsystem.R
+import com.example.parkingmanagementsystem.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+    private val TAG: String = "SplashActivity"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //Hold 3 Sec and go to Activity by get value from SharedPreferences
+        Handler(Looper.getMainLooper()).postDelayed({
+            finish()
+        }, 3000)
+    }
+}
