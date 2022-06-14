c46c1e4e2e5e7b85e00f1a9cb6d6e88234f4cc29
TANIMUL ISLAM <35636662+Tanimul@users.noreply.github.com>
2022-06-15 01:02:31 +0600

    Some Activity added.

---
+++ app/src/main/java/com/example/parkingmanagementsystem/ui/activities/NotificationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.parkingmanagementsystem.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.parkingmanagementsystem.R
+
+class NotificationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notification)
+    }
+}
--- app/src/main/java/com/example/parkingmanagementsystem/ui/main/HomeActivity.kt
+++ app/src/main/java/com/example/parkingmanagementsystem/ui/main/HomeActivity.kt
@@ -1,16 +1,22 @@
 package com.example.parkingmanagementsystem.ui.main
 
 
+import android.annotation.SuppressLint
+import android.app.AlertDialog
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.view.menu.MenuBuilder
 import com.example.parkingmanagementsystem.R
 import com.example.parkingmanagementsystem.data.model.response.User
 import com.example.parkingmanagementsystem.databinding.ActivityHomeBinding
 import com.example.parkingmanagementsystem.databinding.NavHeaderLayoutBinding
 import com.example.parkingmanagementsystem.ui.AppBaseActivity
 import com.example.parkingmanagementsystem.utils.Variables.user
+import com.example.parkingmanagementsystem.utils.extentions.getSharedPrefInstance
 import com.example.parkingmanagementsystem.utils.extentions.loadImageFromUrl
+import com.google.firebase.auth.FirebaseAuth
 
 class HomeActivity : AppBaseActivity() {
     companion object {
@@ -19,6 +25,7 @@ class HomeActivity : AppBaseActivity() {
 
     private lateinit var binding: ActivityHomeBinding
     private lateinit var navHeaderLayoutBinding: NavHeaderLayoutBinding
+    private lateinit var mAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
@@ -26,6 +33,9 @@ class HomeActivity : AppBaseActivity() {
 
         setToolbarWithoutBackButton(binding.layoutToolbar.toolbar)
         title = getString(R.string.home)
+
+        //init
+        mAuth = FirebaseAuth.getInstance()
 
         val actionBarDrawerToggle = ActionBarDrawerToggle(
             this, binding.layoutDrawer,
@@ -44,10 +54,39 @@ class HomeActivity : AppBaseActivity() {
 
     }
 
+    @SuppressLint("RestrictedApi")
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.top_menu, menu)
+        if (menu is MenuBuilder) {
+            menu.setOptionalIconsVisible(true)
+        }
+        return true
+    }
+
     private fun setHeaderInformation(user: User) {
         navHeaderLayoutBinding.tvHeaderName.text = user.name
         navHeaderLayoutBinding.tvHeaderPhoneNumber.text = user.phoneNumber
         navHeaderLayoutBinding.ivHeaderUser.loadImageFromUrl(user.imageUrl)
 
     }
+
+    private fun logOut() {
+        AlertDialog.Builder(this@HomeActivity)
+            .setTitle("Log Out")
+            .setMessage("Are you sure you want to Logged Out?")
+            .setPositiveButton(
+                "OK"
+            ) { _, _ ->
+
+                mAuth.signOut()
+                finish()
+            }
+            .setNegativeButton(
+                "CANCEL"
+            ) { dialog, _ -> dialog.dismiss() }
+            .show()
+
+    }
+
 }
