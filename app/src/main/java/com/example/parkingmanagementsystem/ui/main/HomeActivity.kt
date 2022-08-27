39e5402e55a6ccbd7f8a1e5e8585c0bd0db904be
TANIMUL ISLAM <35636662+Tanimul@users.noreply.github.com>
2022-08-27 22:40:35 +0600

    Map Reinitialize HomeActivity.kt

--- app/src/main/java/com/example/parkingmanagementsystem/ui/main/HomeActivity.kt
+++ app/src/main/java/com/example/parkingmanagementsystem/ui/main/HomeActivity.kt
@@ -3,17 +3,17 @@ package com.example.parkingmanagementsystem.ui.main
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.app.AlertDialog
-import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.graphics.Color
+import android.location.Geocoder
 import android.location.Location
-import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.view.menu.MenuBuilder
 import androidx.core.app.ActivityCompat
@@ -26,7 +26,6 @@ import com.example.parkingmanagementsystem.ui.AppBaseActivity
 import com.example.parkingmanagementsystem.ui.activities.NotificationActivity
 import com.example.parkingmanagementsystem.ui.activities.UsePromoCodeActivity
 import com.example.parkingmanagementsystem.utils.Constants.FirebaseKeys.KEY_USERS_COLLECTION
-import com.example.parkingmanagementsystem.utils.Variables.user
 import com.example.parkingmanagementsystem.utils.extentions.launchActivity
 import com.example.parkingmanagementsystem.utils.extentions.loadImageFromUrl
 import com.google.android.gms.location.FusedLocationProviderClient
@@ -35,15 +34,20 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.CircleOptions
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.Autocomplete
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
 import com.google.android.material.navigation.NavigationBarView
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
+import java.util.*
 
 class HomeActivity : AppBaseActivity(), OnMapReadyCallback,
     NavigationView.OnNavigationItemSelectedListener,
@@ -54,12 +58,17 @@ class HomeActivity : AppBaseActivity(), OnMapReadyCallback,
 
     private lateinit var binding: ActivityHomeBinding
     private lateinit var navHeaderLayoutBinding: NavHeaderLayoutBinding
+
     private lateinit var mAuth: FirebaseAuth
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private val REQUEST_CODE = 101
+
     lateinit var cur_location: Location
     private lateinit var mMap: GoogleMap
+
+    private var marker: Marker? = null
     val db = Firebase.firestore
+    private var addressName: String = "Current Location"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
@@ -70,6 +79,9 @@ class HomeActivity : AppBaseActivity(), OnMapReadyCallback,
 
         //init
         mAuth = FirebaseAuth.getInstance()
+
+        //PLaces API Declare
+        initPlacesAPI()
 
         val actionBarDrawerToggle = ActionBarDrawerToggle(
             this, binding.layoutDrawer,
@@ -87,6 +99,19 @@ class HomeActivity : AppBaseActivity(), OnMapReadyCallback,
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
+        //current location face
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        val mapFragment =
+            supportFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment?
+        mapFragment?.getMapAsync(this@HomeActivity)
+        fetchLastLocation()
+
+
+
+        binding.navBar.setNavigationItemSelectedListener(this)
+    }
+
+    private fun fetchLastLocation() {
         if (ActivityCompat.checkSelfPermission(
                 this,
                 Manifest.permission.ACCESS_FINE_LOCATION
@@ -99,18 +124,20 @@ class HomeActivity : AppBaseActivity(), OnMapReadyCallback,
             )
             return
         }
+
         fusedLocationClient.lastLocation
             .addOnSuccessListener {
                 // Got last known location. In some rare situations this can be null.
+                Log.d(TAG, "onCreate: $it")
                 if (it != null) {
                     cur_location = it
+                    moveCamera(cur_location)
                 }
-                val mapFragment = supportFragmentManager
-                    .findFragmentById(R.id.map_fragment) as? SupportMapFragment
-                mapFragment?.getMapAsync(this)
             }
+    }
 
-        binding.navBar.setNavigationItemSelectedListener(this)
+    private fun initPlacesAPI() {
+        Places.initialize(applicationContext, "" + resources.getString(R.string.map_Api_key));
     }
 
     @SuppressLint("RestrictedApi")
@@ -157,20 +184,79 @@ class HomeActivity : AppBaseActivity(), OnMapReadyCallback,
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
         mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
-        val currentLocation = LatLng(cur_location.latitude, cur_location.longitude)
-        Log.d(TAG, "onMapReady: $currentLocation")
-        mMap.addMarker(
-            MarkerOptions().position(currentLocation).title("Cur_location")
-        )
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 7f))
-        mMap.addCircle(
-            CircleOptions()
-                .center(currentLocation)
-                .radius(10000.0)
-                .strokeColor(Color.RED)
-                .fillColor(Color.argb(70, 150, 50, 50))
-        )
+
+        mMap.setOnMapClickListener {
+            val latLng = LatLng(it.latitude, it.longitude)
+            moveCamera(latLng)
+            val geocoder =
+                Geocoder(this, Locale.getDefault())
+            val addressList =
+                geocoder.getFromLocation(it.latitude, it.longitude, 1)
+            addressName = addressList[0].getAddressLine(0)
+
+        }
     }
+
+    private fun moveCamera(latLng: LatLng) {
+        marker?.remove()
+        marker = mMap.addMarker(
+            MarkerOptions()
+                .position(latLng)
+                .title("" + addressName)
+        )!!
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng))
+    }
+
+    private fun moveCamera(location: Location) {
+        val latLng = LatLng(location.latitude, location.longitude)
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng))
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f))
+
+        marker?.remove()
+        marker = mMap.addMarker(
+            MarkerOptions()
+                .position(latLng)
+                .title("" + addressName)
+        )!!
+
+//        mMap.addCircle(
+//            CircleOptions()
+//                .center(currentLocation)
+//                .radius(10000.0)
+//                .strokeColor(Color.RED)
+//                .fillColor(Color.argb(70, 150, 50, 50))
+//        )
+    }
+
+    private fun searchPlaces() {
+        val fields: List<Place.Field> = Arrays.asList(
+            Place.Field.ID, Place.Field.NAME, Place.Field.ADDRESS, Place.Field.LAT_LNG,
+            Place.Field.TYPES
+        )
+
+        val intent = Autocomplete.IntentBuilder(
+            AutocompleteActivityMode.OVERLAY, fields
+        )
+            .setCountry("bd")
+            .setHint("Search for Places")
+            .build(this@HomeActivity)
+
+        activityPlacesResultLauncher.launch(intent)
+
+    }
+
+    var activityPlacesResultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                // There are no request codes
+                val data: Intent? = result.data
+                val place: Place = Autocomplete.getPlaceFromIntent(data)
+                Log.d(TAG, "place address: " + place.address)
+                addressName = place.name
+                val latLng = LatLng(place.latLng.latitude, place.latLng.longitude)
+                moveCamera(latLng)
+            }
+        }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
@@ -182,6 +268,7 @@ class HomeActivity : AppBaseActivity(), OnMapReadyCallback,
         }
         return true
     }
+
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
 
