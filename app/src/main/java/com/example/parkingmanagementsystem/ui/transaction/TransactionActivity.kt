d9d064da7591a4b69cad534ebb22c34dac003860
TANIMUL ISLAM <35636662+Tanimul@users.noreply.github.com>
2022-09-09 23:35:26 +0600

    Transaction Activity added.

---
+++ app/src/main/java/com/example/parkingmanagementsystem/ui/transaction/TransactionActivity.kt
@@ -0,0 +1,12 @@
+package com.example.parkingmanagementsystem.ui.transaction
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.parkingmanagementsystem.R
+
+class TransactionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_transaction)
+    }
+}
