6af424310c9e3754bcce1f5b9d07ee14b6c30a37
TANIMUL ISLAM <35636662+Tanimul@users.noreply.github.com>
2022-09-10 16:48:57 +0600

    Distance Calculate

--- app/src/main/java/com/example/parkingmanagementsystem/adapter/MonthlyParkingListAdapter.kt
+++ app/src/main/java/com/example/parkingmanagementsystem/adapter/MonthlyParkingListAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.parkingmanagementsystem.adapter
 
 
+import android.location.Location
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -9,10 +10,14 @@ import com.example.parkingmanagementsystem.data.model.response.NotificationInfo
 import com.example.parkingmanagementsystem.databinding.LayoutMonthlyParkingBinding
 import com.example.parkingmanagementsystem.databinding.LayoutNotificationBinding
 import com.example.parkingmanagementsystem.utils.extentions.loadImageFromUrl
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.SphericalUtil
 import java.text.SimpleDateFormat
 
 class MonthlyParkingListAdapter(
     private val mMonthlyParkingItems: List<MonthlyParkingInfo>,
+    private val lat: Double,
+    private val lon: Double,
 ) :
     RecyclerView.Adapter<MonthlyParkingListAdapter.FoodListViewHolder>() {
     private val TAG = "MonthlyParkingListAdapter"
@@ -35,12 +40,25 @@ class MonthlyParkingListAdapter(
         with(holder.binding) {
             with(mMonthlyParkingItems[position]) {
                 tvAddress.text = placeAddress
-                tvAmount.text = ultimateCost
+                tvAmount.text = "$ultimateCost BDT"
                 ivPlace.loadImageFromUrl(placeUrl)
+                tvDistance.text =
+                    String.format(
+                        "%.2f",
+                        tvDistance(
+                            LatLng(lat, lon),
+                            LatLng(placeLatitude, placeLongitude)
+                        ) / 1000
+                    ) + " km"
             }
         }
 
 
+    }
+
+    private fun tvDistance(latLng: LatLng, latLng1: LatLng): Double {
+        val distance = SphericalUtil.computeDistanceBetween(latLng, latLng1)
+        return distance
     }
 
     override fun getItemCount(): Int {
