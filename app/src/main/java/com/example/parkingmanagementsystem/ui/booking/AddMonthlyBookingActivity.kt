6c005ccce8355a7da58384d97aa45345f632a224
TANIMUL ISLAM <35636662+Tanimul@users.noreply.github.com>
2022-09-14 23:02:20 +0600

    Add Monthly Booking Design

---
+++ app/src/main/java/com/example/parkingmanagementsystem/ui/booking/AddMonthlyBookingActivity.kt
@@ -0,0 +1,11 @@
+package com.example.parkingmanagementsystem.ui.booking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddMonthlyBookingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_monthly_booking)
+    }
+}
