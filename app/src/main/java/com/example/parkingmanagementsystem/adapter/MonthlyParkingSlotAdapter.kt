90db7e01fa5d994fbf067814970d72b2b71242b8
TANIMUL ISLAM <35636662+Tanimul@users.noreply.github.com>
2022-10-15 00:40:58 +0600

    Add Monthly booking updated

---
+++ app/src/main/java/com/example/parkingmanagementsystem/adapter/MonthlyParkingSlotAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.parkingmanagementsystem.adapter
+
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.parkingmanagementsystem.data.listener.MonthlyParkingSlotOnClickListener
+import com.example.parkingmanagementsystem.databinding.LayoutSlotBinding
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.SphericalUtil
+
+class MonthlyParkingSlotAdapter(
+    private val mMonthlyParkingTime: String,
+    private val mMonthlyParkingCost: String,
+    private val mMonthlyParkingSlot: String,
+    private val monthlyParkingSlotOnClickListener: MonthlyParkingSlotOnClickListener
+) :
+    RecyclerView.Adapter<MonthlyParkingSlotAdapter.FoodListViewHolder>() {
+    private val TAG = "MonthlyParkingListAdapter"
+
+    inner class FoodListViewHolder(val binding: LayoutSlotBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FoodListViewHolder {
+        return FoodListViewHolder(
+            LayoutSlotBinding.inflate(
+                LayoutInflater.from(parent!!.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: FoodListViewHolder, position: Int) {
+        with(holder.binding) {
+         tvTime.text=mMonthlyParkingTime
+         tvPrice.text=mMonthlyParkingCost
+        }
+
+        holder.binding.checkboxMeat.setOnClickListener {
+            monthlyParkingSlotOnClickListener.onItemClick( position)
+        }
+
+
+    }
+
+
+    override fun getItemCount(): Int {
+        return mMonthlyParkingSlot.toInt()
+    }
+}
--- app/src/main/java/com/example/parkingmanagementsystem/ui/user/monthly_parking/AddMonthlyBookingActivity.kt
+++ app/src/main/java/com/example/parkingmanagementsystem/ui/user/monthly_parking/AddMonthlyBookingActivity.kt
@@ -1,12 +1,92 @@
 package com.example.parkingmanagementsystem.ui.user.monthly_parking
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.parkingmanagementsystem.R
+import com.example.parkingmanagementsystem.adapter.MonthlyParkingSlotAdapter
+import com.example.parkingmanagementsystem.data.listener.MonthlyParkingSlotOnClickListener
+import com.example.parkingmanagementsystem.data.model.response.MonthlyParkingInfo
+import com.example.parkingmanagementsystem.databinding.ActivityAddMonthlyBookingBinding
+import com.example.parkingmanagementsystem.ui.AppBaseActivity
+import com.example.parkingmanagementsystem.utils.extentions.loadImageFromUrl
+import com.example.parkingmanagementsystem.utils.extentions.toast
 
-class AddMonthlyBookingActivity : AppCompatActivity() {
+class AddMonthlyBookingActivity : AppBaseActivity(), MonthlyParkingSlotOnClickListener {
+    companion object {
+        private const val TAG: String = "AddMonthlyBookingActivity"
+    }
+
+    private lateinit var binding: ActivityAddMonthlyBookingBinding
+
+    lateinit var itemResponse: MonthlyParkingInfo
+    private lateinit var monthlyParkingSlotAdapter: MonthlyParkingSlotAdapter
+    private lateinit var slot: ArrayList<Int>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_monthly_booking)
+        binding = ActivityAddMonthlyBookingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        intent.getSerializableExtra("monthlyParkingInfo")?.let {
+            val monthlyParkingInfo = it as MonthlyParkingInfo
+            itemResponse = MonthlyParkingInfo(
+                uploaderId = monthlyParkingInfo.uploaderId,
+                key = monthlyParkingInfo.key,
+                placeName = monthlyParkingInfo.placeName,
+                placeAddress = monthlyParkingInfo.placeAddress,
+                placeLatitude = monthlyParkingInfo.placeLatitude,
+                placeLongitude = monthlyParkingInfo.placeLongitude,
+                month = monthlyParkingInfo.month,
+                time = monthlyParkingInfo.time,
+                ultimateCost = monthlyParkingInfo.ultimateCost,
+                totalParkingSpace = monthlyParkingInfo.totalParkingSpace,
+                placeUrl = monthlyParkingInfo.placeUrl,
+                addedAt = monthlyParkingInfo.addedAt,
+                updatedAt = monthlyParkingInfo.updatedAt
+            )
+            slot = ArrayList<Int>()
+            setValue()
+
+        }
+
+
+        monthlyParkingSlotAdapter = MonthlyParkingSlotAdapter(
+            itemResponse.time,
+            itemResponse.ultimateCost,
+            itemResponse.totalParkingSpace,
+            this
+        )
+        binding.rvBooking.adapter = monthlyParkingSlotAdapter
+
+        val monthlyParkingSlotListLayoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        binding.rvBooking.layoutManager = monthlyParkingSlotListLayoutManager
+
+
+        binding.btnBooking.setOnClickListener {
+            if(slot.isNotEmpty()){
+
+            }else{
+                toast("Please add minimum one slot")
+            }
+        }
+
     }
+
+    private fun setValue() {
+        binding.ivPlace.loadImageFromUrl(itemResponse.placeUrl)
+        binding.tvAddress.text = itemResponse.placeAddress
+        binding.tvMonth.text = itemResponse.month
+    }
+
+    override fun onItemClick(position: Int) {
+        if(slot.contains(position)){
+            slot.remove(position)
+        }else{
+            slot.add(position)
+        }
+    }
+
+
 }
