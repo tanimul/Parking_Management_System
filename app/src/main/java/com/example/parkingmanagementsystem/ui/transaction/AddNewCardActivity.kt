82c5fadc8e628e421d20f8dd669bbd6e1efde69a
TANIMUL ISLAM <35636662+Tanimul@users.noreply.github.com>
2022-10-18 00:45:59 +0600

    Add New Card to local database.

--- app/src/main/java/com/example/parkingmanagementsystem/ui/transaction/AddNewCardActivity.kt
+++ app/src/main/java/com/example/parkingmanagementsystem/ui/transaction/AddNewCardActivity.kt
@@ -1,12 +1,56 @@
 package com.example.parkingmanagementsystem.ui.transaction
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.parkingmanagementsystem.R
+import com.example.parkingmanagementsystem.data.model.response.CardModel
+import com.example.parkingmanagementsystem.databinding.ActivityAddNewCardBinding
+import com.example.parkingmanagementsystem.ui.AppBaseActivity
+import com.example.parkingmanagementsystem.utils.Constants.REQUEST_CODE_ADD_CARD
+import com.example.parkingmanagementsystem.utils.extentions.toast
+import java.io.Serializable
 
-class AddNewCardActivity : AppCompatActivity() {
+
+class AddNewCardActivity : AppBaseActivity() {
+    companion object {
+        private const val TAG: String = "AddNewCardActivity"
+    }
+
+    private lateinit var binding: ActivityAddNewCardBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_new_card)
+        binding=ActivityAddNewCardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnAddCard.setOnClickListener {
+            addCard()
+        }
+    }
+
+    private fun addCard() {
+        if (binding.etCardNumber.text.toString().isEmpty() && binding.etCardName.text.toString()
+                .isEmpty() && binding.etCvv.text.toString()
+                .isEmpty()
+        ) {
+            toast("Please Fill all the information's")
+        } else {
+
+            val cardModel = CardModel(
+                cardName = binding.etCardName.text.toString(),
+                cardNumber = binding.etCardNumber.text.toString(),
+                cvv = binding.etCvv.text.toString(),
+            )
+            val resultIntent = Intent()
+            setResult(
+                REQUEST_CODE_ADD_CARD,
+                resultIntent.putExtra("cardModel", cardModel as Serializable)
+            )
+            Log.d(TAG, "addCard: ")
+
+        }
+
+        finish()
     }
 }
